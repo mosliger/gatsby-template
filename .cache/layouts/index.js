bbf43383a60f9ba88823f70eb7d337828fe4f353
chonlatee thomkum <liger200436@gmail.com>
2017-11-15 14:16:58 +0700

    build

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/xampp/htdocs/gatsby/src/layouts/index.js"
+  import data from "C:\\xampp\\htdocs\\gatsby\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
